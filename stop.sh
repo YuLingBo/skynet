bdab5d744875d56ef1e6a553a4cb39de59daa30b
yyy <yulingbo1990@outlook.com>
2024-12-18 22:21:02 +0800

    update stop.sh

--- stop.sh
+++ stop.sh
@@ -4,9 +4,3 @@ ps  -efww|grep skynet|grep -v grep|cut -c 9-15|xargs kill -9
 # 查询所有占用端口
 netstat -unltp
 
-USE mysql;
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456';
-FLUSH PRIVILEGES;
-
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY '123456';
